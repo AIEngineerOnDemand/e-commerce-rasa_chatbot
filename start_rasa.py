dc79340863133bbc8465d2a7e4ddd29287c80469
AIEngineerOnDemand <giopantana@gmail.com>
2023-12-16 14:08:47 -0500

    Add start_rasa.py script to run Rasa server with environment variables to connect to postgre db

---
+++ start_rasa.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+import os
+import subprocess
+
+# Load environment variables
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+dotenv_path = os.path.join(parent_dir, '.env')
+
+load_dotenv(dotenv_path)
+
+# Start Rasa server with enable-api, debug, and cors arguments
+subprocess.call(['rasa', 'run', '--enable-api', '--debug', '--cors', '*'])
